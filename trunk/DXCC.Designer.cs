1ac926dc7a6d5077735ffdab59261108ba1bd3a6
yt7pwr@gmail.com <yt7pwr@gmail.com@60043531-e44d-f362-7009-5c89066a118d>
2012-12-23 15:48:37 +0000

    23122012 - DXCC added QRB and AZ calculation

--- trunk/DXCC.Designer.cs
+++ trunk/DXCC.Designer.cs
@@ -30,22 +30,30 @@
         {
             this.rtbDXCC = new System.Windows.Forms.RichTextBox();
             this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // rtbDXCC
             // 
             this.rtbDXCC.BackColor = System.Drawing.Color.Black;
+            this.rtbDXCC.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.rtbDXCC.ForeColor = System.Drawing.Color.LawnGreen;
-            this.rtbDXCC.Location = new System.Drawing.Point(32, 21);
+            this.rtbDXCC.Location = new System.Drawing.Point(24, 29);
             this.rtbDXCC.Name = "rtbDXCC";
-            this.rtbDXCC.Size = new System.Drawing.Size(379, 173);
+            this.rtbDXCC.Size = new System.Drawing.Size(400, 173);
             this.rtbDXCC.TabIndex = 0;
             this.rtbDXCC.Text = "";
             this.rtbDXCC.TextChanged += new System.EventHandler(this.rtbDXCC_TextChanged);
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(187, 211);
+            this.btnClear.Location = new System.Drawing.Point(187, 213);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
             this.btnClear.TabIndex = 1;
@@ -53,12 +61,89 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(55, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CALL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(111, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Country";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(230, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Zone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(274, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "ITU";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(314, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "AZ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(352, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "QRB";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(166, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Continent";
+            // 
             // DXCC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(448, 250);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.rtbDXCC);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -68,9 +153,9 @@
             this.MinimumSize = new System.Drawing.Size(458, 282);
             this.Name = "DXCC";
             this.Text = "DXCC";
-            this.Load += new System.EventHandler(this.DXCC_Load);
-            this.ResumeLayout(false);
             this.Closing += new System.ComponentModel.CancelEventHandler(this.DXCC_Closing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,6 +163,13 @@
 
         public System.Windows.Forms.RichTextBox rtbDXCC;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
 
     }
 }
